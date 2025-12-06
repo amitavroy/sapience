ceb3469c3827bc99ab09d3765f376ffe6e3cbcec
Amitav Roy <reachme@amitavroy.com>
2025-12-06 07:42:01 +0000

    chore: Update DatabaseSeeder to create users and organisations with roles, and add new script commands in composer.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Organisation;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,15 +14,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
+        $user = User::firstOrCreate(
+            ['email' => 'reachme@amitavroy.com'],
             [
-                'name' => 'Test User',
-                'password' => 'password',
+                'name' => 'Amitav Roy',
+                'password' => 'Password@123',
                 'email_verified_at' => now(),
             ]
         );
+
+        // Create an organisation and attach the user to it
+        $organisation = Organisation::firstOrCreate([
+            'name' => 'Devzone',
+        ]);
+        $organisation->users()->attach($user->id, ['role' => 'admin']);
+
+        // Johon Doe user
+        $johnDoe = User::firstOrCreate([
+            'email' => 'john.doe@example.com',
+            'name' => 'John Doe',
+            'password' => 'Password@123',
+            'email_verified_at' => now(),
+        ]);
+
+        // Create an organisation and attach the user to it
+        $organisation2 = Organisation::firstOrCreate([
+            'name' => 'Jhon Company',
+        ]);
+        $organisation2->users()->attach($johnDoe->id, ['role' => 'member']);
+
+        // Add $user to $organisation2 as a member
+        $organisation2->users()->attach($user->id, ['role' => 'member']);
     }
 }
